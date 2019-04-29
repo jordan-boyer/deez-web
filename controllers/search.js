ccb0cdbb603334d18c49d30b9c1ac141f13b02f2
Jordan Boyer <boyer.jordan42@gmail.com>
2019-04-29 15:28:56 +0200

    add API call on search page + form(no styling)

--- controllers/search.js
+++ controllers/search.js
@@ -1,3 +1,8 @@
+//Import
+import {getMusicsFromDeezer} from "../utils/deezer-api";
+import $ from "jquery";
+
+//Class definition for page Search
 export default class Search {
 
     constructor() {
@@ -5,6 +10,41 @@ export default class Search {
     }
 
     init() {
+        $(function() {
+            const $form = $("form");
+            const $title = $form.find("#title");
+            const $tri = $form.find("#tri");
+            const $result = $("#resultSection");
 
+            //Add music to the page
+            function addMusic(music) {
+                $result.html(`<img src="${music.album.cover_medium}">
+                <h4>${music.title}</h4>
+                <h5>${music.artist.name} / ${music.album.title}</h5>
+                <audio controls src="${music.preview}">Your browser does not support the<code>audio</code> element.</audio>`)
+            }
+
+            $form.submit((e) => {
+                //prevent submit of form
+                e.preventDefault();
+
+                const title = $title.val();
+                const tri = $tri.val();
+
+                //get all music base on data
+                getMusicsFromDeezer(title, tri)
+                    .then(musiques => {
+                        if (musiques.data.length !== 0) {
+                            addMusic(musiques.data[0]);
+                        } else {
+                            $result.html("Nous sommes désolé, nous n'obtenons pas de résultats pour cette recherche...");
+                        }  
+                    }).catch(error => {
+                        $result.html(`Nous sommes désolé mais une erreur est parvenue, vous pouvez recharger la page et essayer de nouveau
+                        Si le problème persiste rendez-vous sur <a href="https://github.com/jordan-boyer/deez-web">cette page</a> pour nous contactez`);
+                    });
+            });
+            
+        });
     }
 };
---
+++ utils/deezer-api.js
@@ -0,0 +1,22 @@
+//Import 
+import fetchJsonp from "fetch-jsonp";
+
+//Const definition for API URL
+const apiBaseURL = "https://api.deezer.com/";
+const searchOption = "search?q=";
+const orderOption = "order=";
+const output = "output=jsonp";
+
+
+//Get music from deezer API || return promise
+export function getMusicsFromDeezer(title, order) {
+
+    // encodeURIComponent against malicious data
+    title = encodeURIComponent(title);
+    order = encodeURIComponent(order);
+
+    // fetchJsonp because deezer API doesn't support fecth (origin not allow)
+    return fetchJsonp(`${apiBaseURL}${searchOption}${title}&${orderOption}${order}&${output}`)
+    .then(response => response.json());
+}
+
